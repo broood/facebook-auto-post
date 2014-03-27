2c51b595b03314a9f66b5e921681040ada235979
Grant Gill <grantdgill@gmail.com>
2014-03-27 10:19:35 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var request = require("request");
+var baseUrl = "https://graph.facebook.com/";
+
+var facebook = (function() {
+	return {
+		post: function(obj) {
+			obj = obj || {};
+			var callback = obj.callback || function(){};
+			var message = obj.message;
+			var pageId = obj.pageId;
+			var accessToken = obj.accessToken;
+
+			if(message && pageId && accessToken) {
+				var url = baseUrl + pageId + "/feed";
+
+				var body = "message=" + message + "&access_token=" + accessToken;
+
+				request({
+					url:url,
+					method:"POST",
+					headers:null,
+					body:body
+				}, function(err, response, body) {
+					if(err) {
+						callback(err);
+					} else {
+						callback(null, body);
+					}
+				});
+			} else {
+				callback("Message, pageId and accessToken must all be provided");
+			}
+		}
+	};
+})();
+
+module.exports = facebook;
